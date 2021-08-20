3c8d11b009ae160314b43726813429c11d250711
Ryan Michaels <rmichaels@eab.com>
2021-08-20 10:51:46 -0400

    initial commit of a base

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,10 @@
 /** @type {import('@sveltejs/kit').Config} */
+import preprocess from 'svelte-preprocess'
 const config = {
+	preprocess: preprocess({}),
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		target: '#app'
 	}
 };
 
