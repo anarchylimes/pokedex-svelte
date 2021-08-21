ee1da420228cfba41ee67b869089de4c86872413
Ryan M <46380964+anarchylimes@users.noreply.github.com>
2021-08-21 00:13:18 -0400

    change adapter to static
    change adapter to static

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,19 @@
 /** @type {import('@sveltejs/kit').Config} */
+
 import preprocess from 'svelte-preprocess'
+import adapter from '@sveltejs/adapter-static';
+
+
 const config = {
 	preprocess: preprocess({}),
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#app'
+		target: '#app',
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: null
+		})
 	}
 };
 
