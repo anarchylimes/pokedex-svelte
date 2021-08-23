3a987708e922d9fb909c71b5a90cba59b6827e21
Ryan Michaels <rmichaels@eab.com>
2021-08-23 12:06:47 -0400

    Wow big commit.
    Moved pokemon to a component.
    changed how we access images

--- src/components/PokemonCard.svelte
+++ src/components/PokemonCard.svelte
@@ -1,11 +1,39 @@
 <script>
-	export let name
-  export let url
-
-  const Array = url.split('/')
-
+  export let name
+  export let img
 </script>
 
-<style>
-	/* your styles go here */
+<style lang="scss">
+  @import '../styles/_settings';
+		.pokemon {
+      background: $white;
+			max-width: 16rem;
+			width: 100%;
+			display: flex;
+			flex-direction: column;
+			justify-content: center;
+			align-items: center;
+			margin: 1rem 0.5rem;
+			padding: 1rem;
+			border-radius: 8px;
+			box-shadow: rgba(149, 157, 165, 0.2) 0px 4px 12px;
+		}
+
+		.pokemon-image {
+			height: 14rem;
+		}
+
+		.pokemon-name {
+      margin-top: 1.5rem;
+			text-transform: capitalize; 
+      font-family: 'IBM Plex Serif', serif;
+      font-style: italic;
+      font-size: 1.25rem;
+      letter-spacing: 1px;
+		}
 </style>
+
+<article class="pokemon {name}">
+    <img class="pokemon-image" src={img} alt="{name}">
+    <p class="pokemon-name">{name}</p>
+</article>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,29 +1,32 @@
 <script>
+	import PokemonCard from '../components/PokemonCard.svelte';
+
 	import { onMount } from 'svelte';
 
-	import { pokemonStorage } from './store';
+	import { pokemonStorage } from './storage';
 
 	const numberOfPokemon = 151;
 	let allPokemonInfo;
 	let pokemons = [];
 
 	pokemonStorage.subscribe((value) => {
-    allPokemonInfo = value
-  });
+		allPokemonInfo = value;
+	});
 
 	onMount(async () => {
 		let baseApi = `https://pokeapi.co/api/v2/pokemon/?limit=${numberOfPokemon}`;
-		const data = await fetch(baseApi)
-    const results = await data.json()
-		pokemons = results.results
+		const data = await fetch(baseApi);
+		const results = await data.json();
+		pokemons = results.results;
 
-    for (const pokemon of pokemons) {
-      const pokemonFetchRequest = await fetch(pokemon.url);
-      const singlePokemonInfo = await pokemonFetchRequest.json();
-      pokemonStorage.update((value) => [...value, singlePokemonInfo]);
-    }
+		for (const pokemon of pokemons) {
+			const pokemonFetchRequest = await fetch(pokemon.url);
+			const singlePokemonInfo = await pokemonFetchRequest.json();
+			pokemonStorage.update((value) => [...value, singlePokemonInfo]);
+		}
 
-    pokemonStorage.update(() => [...allPokemonInfo])
+		pokemonStorage.update(() => [...allPokemonInfo]);
+		console.log(allPokemonInfo);
 	});
 </script>
 
@@ -34,10 +37,10 @@
 <div id="container">
 	<div id="grid">
 		{#each allPokemonInfo as pokemon}
-			<div class="pokemon {pokemon.name}">
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/{pokemon.id}.png" alt="{pokemon.name} artwork" class="pokemon-image" />
-				<p class="pokemon-name">{pokemon.name}</p>
-			</div>
+			<PokemonCard
+				name={pokemon.name}
+				img={pokemon['sprites']['other']['official-artwork']['front_default']}
+			/>
 		{/each}
 	</div>
 </div>
@@ -55,32 +58,5 @@
 		justify-content: space-around;
 		align-items: center;
 		flex-wrap: wrap;
-
-		.pokemon {
-			background: $white;
-			max-width: 16rem;
-			width: 100%;
-			display: flex;
-			flex-direction: column;
-			justify-content: center;
-			align-items: center;
-			margin: 1rem 0.5rem;
-			padding: 1rem;
-			border-radius: 8px;
-			box-shadow: rgba(149, 157, 165, 0.2) 0px 4px 12px;
-		}
-
-		.pokemon-image {
-			height: 14rem;
-		}
-
-		.pokemon-name {
-      margin-top: 1.5rem;
-			text-transform: capitalize; 
-      font-family: 'Libre Baskerville', serif;
-      font-style: italic;
-      font-size: 20px;
-      letter-spacing: 1px;
-		}
 	}
 </style>
